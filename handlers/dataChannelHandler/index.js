72bb34675cdcb498a7ecd19c4ad4bd44fff41865
Joshua-Wright76 <jwrightbluj@gmail.com>
2025-08-23 21:18:24 -0700

    fixed tabs in datachannelhandler

--- handlers/dataChannelHandler/index.js
+++ handlers/dataChannelHandler/index.js
@@ -5,32 +5,41 @@ const { constants } = require("../../utils/constants");
 
 const getData = async (guild) => {
   const channels = await guild.channels.fetch();
-  const dataChannel = channels.find((channel) => channel.name === constants.DATA_CHANNEL_NAME);
+  const dataChannel = channels.find(
+    (channel) => channel.name === constants.DATA_CHANNEL_NAME
+  );
   if (!dataChannel) return;
   const dataChannelMessages = await dataChannel.messages.fetch({ limit: 100 });
-  const dailyMessagesSettingsMessage = dataChannelMessages
-    .find((message) => message.content.includes('settings for the daily messages'));
+  const dailyMessagesSettingsMessage = dataChannelMessages.find((message) =>
+    message.content.includes("settings for the daily messages")
+  );
   if (!dailyMessagesSettingsMessage) return;
-  const dailyMessagesSettingsString = `{${dailyMessagesSettingsMessage.content.split('{')[1].split('}')[0]}}`
-    .split('\n').join('');
+  const dailyMessagesSettingsString = `{${
+    dailyMessagesSettingsMessage.content.split("{")[1].split("}")[0]
+  }}`
+    .split("\n")
+    .join("");
 
   try {
-	  const dailyMessagesSettings = JSON.parse(
-	    dailyMessagesSettingsString
-	  );
-	  const birthdaysMessage = dataChannelMessages
-	    .find((message) => message.content.includes('list of birthdays'));
-	  const birthdaysString = `{${birthdaysMessage.content.split('{')[1].split('}')[0].split('\n').join('')}}`;
-	  const birthdays = JSON.parse(birthdaysString);
-	  return {
-	    dailyMessagesSettings,
-	    birthdays
-	  };
+    const dailyMessagesSettings = JSON.parse(dailyMessagesSettingsString);
+    const birthdaysMessage = dataChannelMessages.find((message) =>
+      message.content.includes("list of birthdays")
+    );
+    const birthdaysString = `{${birthdaysMessage.content
+      .split("{")[1]
+      .split("}")[0]
+      .split("\n")
+      .join("")}}`;
+    const birthdays = JSON.parse(birthdaysString);
+    return {
+      dailyMessagesSettings,
+      birthdays,
+    };
   } catch (error) {
-	  console.error(error)
+    console.error(error);
   }
 };
 
 module.exports = {
-  getData
+  getData,
 };
